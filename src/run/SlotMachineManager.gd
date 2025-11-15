facf61beb38f9df68f460f0e4ab2ff19002348bc
saveligulas <saveli.gulas@gmail.com>
2025-11-15 11:07:59 +0100

    statussy

--- src/run/SlotMachineManager.gd
+++ src/run/SlotMachineManager.gd
@@ -4,13 +4,13 @@ extends RefCounted
 var result_grid: Array = []  # 5x3 grid
 
 func _init():
-    result_grid.resize(5)
-    for i in range(5):
-        result_grid[i] = []
-        result_grid[i].resize(3)
+	result_grid.resize(5)
+	for i in range(5):
+		result_grid[i] = []
+		result_grid[i].resize(3)
 
 func spin(player_data: PlayerData):
-    for column in range(5):
-        for row in range(3):
-            if player_data.reels[column].symbols.size() > 0:
-                result_grid[column][row] = player_data.reels[column].symbols.pick_random()
+	for column in range(5):
+		for row in range(3):
+			if player_data.reels[column].symbols.size() > 0:
+				result_grid[column][row] = player_data.reels[column].symbols.pick_random()
