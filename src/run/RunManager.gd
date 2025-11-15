b0c477a20825a4d9e3f34698aacd0b7b6a32c9ef
saveligulas <saveli.gulas@gmail.com>
2025-11-15 13:02:18 +0100

    brr

--- src/run/RunManager.gd
+++ src/run/RunManager.gd
@@ -5,19 +5,19 @@ extends Node
 var slot_machine_manager: SlotMachineManager
 
 func initialize():
-    slot_machine_manager = SlotMachineManager.new()
+	slot_machine_manager = SlotMachineManager.new()
 
-    if player_data == null:
-        player_data = PlayerData.new()
-        player_data.initialize()
+	if player_data == null:
+		player_data = PlayerData.new()
+		player_data.initialize()
 
-        for reel in player_data.reels:
-            starting_symbols.shuffle()
-            reel.symbols.append_array(starting_symbols)
+		for reel in player_data.reels:
+			starting_symbols.shuffle()
+			reel.symbols.append_array(starting_symbols)
 
 
 func reset():
-    pass  # TODO: Implement reset logic
+	pass  # TODO: Implement reset logic
 
 func _ready():
-    print("I LOADED RUNMANAGER")
+	print("I LOADED RUNMANAGER")
--- src/run/SlotMachineManager.gd
+++ src/run/SlotMachineManager.gd
@@ -4,20 +4,20 @@ extends RefCounted
 var result_grid: Array = []  # 5x3 grid
 
 func _init():
-    result_grid.resize(5)
-    for i in range(5):
-        result_grid[i] = []
-        result_grid[i].resize(3)
+	result_grid.resize(5)
+	for i in range(5):
+		result_grid[i] = []
+		result_grid[i].resize(3)
 
 func spin(player_data: PlayerData):
-    for col in range(5):
-        var reel_symbols = player_data.reels[col].symbols
-        var reel_size = reel_symbols.size()
-        if reel_size == 0:
-            continue
+	for col in range(5):
+		var reel_symbols = player_data.reels[col].symbols
+		var reel_size = reel_symbols.size()
+		if reel_size == 0:
+			continue
 
-        var start_index = randi() % reel_size
+		var start_index = randi() % reel_size
 
-        for row in range(3):
-            var index = (start_index + row) % reel_size
-            result_grid[col][row] = reel_symbols[index]
+		for row in range(3):
+			var index = (start_index + row) % reel_size
+			result_grid[col][row] = reel_symbols[index]
